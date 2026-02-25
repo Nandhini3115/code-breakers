55ab4f3921e3be6b919c3a3c40c4b0a24d0058b7
Nandhini3115 <jnandhini3115@gmail.com>
2026-02-25 21:53:47 +0530

    final changes

--- frontend/src/pages/Round1Task1.jsx
+++ frontend/src/pages/Round1Task1.jsx
@@ -13,14 +13,14 @@ export default function Round1Task1() {
   const [answers, setAnswers] = useState(Array(8).fill(""));
 
   const correctAnswers = [
-    "Y W U S Q O M K I G E C A B D F H J L N P R T V X Z",  // Q1
+    "4321",  // Q1
     "0 1 1 2 3 5 8",    // Q2
     `BREAKERSCODE`,     // Q3
-    "4 10",    // Q4
+    "15 15",    // Q4
     "dEbUG",     // Q5
-    "B C E H M Z H M",     // Q6
-    "1 2 6 24 120 720 5040",    // Q7
-    "12 13 21 23 31 32"      // Q8
+    "12 7",     // Q6
+    "1 2 6 24 120",    // Q7
+    "10 15 20"      // Q8
   ];
 
   const checkAnswer = (input, correct) => {
@@ -92,27 +92,14 @@ export default function Round1Task1() {
   };
 
   const questions = [
-    `letters = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
-s = []
+    `num = 1234
+rev = 0
 
-for i, ch in enumerate(letters):
-    if i % 2 == 0:
-        s.insert(0, ch)  # prepend
-    else:
-        s.append(ch)     # append
+while num > 0:
+    rev = rev * 10 + num % 10
+    num //= 10
 
-print(" ".join(s))`,
-    `n = 7
-a = 0
-b = 1
-
-print(a, b, end=" ")
-
-for i in range(2, n):
-    c = a + b
-    print(c, end=" ")
-    a = b
-    b = c`,
+print(rev)`,
     `def rotate(s, n):
     n = n % len(s)
     return s[n:] + s[:n]
@@ -124,9 +111,14 @@ for i in range(3):
 
 print(text)`,
     `#include <stdio.h>
-int main(){
-    int x = 10;
-    printf("%lu %d", sizeof(x++), x);
+int main() {
+    int a = 10;
+    int *p = &a;
+
+    *p = *p + 5;
+    printf("%d %d", a, *p);
+
+    return 0;
 }`,
     `#include<stdio.h>
 
@@ -144,38 +136,36 @@ int main() {
     printf(str);
 }`,
     `#include <stdio.h>
-int main(){
-    char letters[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    int a=1,b=2,temp;
-    for(int i=0;i<10;i++){
-        printf("%c ", letters[a-1]);
-        temp=a+b;
-        a=b;
-        b=temp;
-        if(a>26) a%=26;
-    }
+int main() {
+    int a = 5;
+    int b = a++ + ++a;
+    printf("%d %d", b, a);
     return 0;
+}
 }`,
     `public class Main {
     public static void main(String[] args) {
         int n = 1;
-        for(int i=1; i<=7; i++){
+        for(int i=1; i<=5; i++){
             n *= i;
             System.out.print(n + " ");
         }
     }
 }`,
-    `public class Main {
-    public static void main(String[] args) {
+    `class Test {
+    static int x = 5;
 
-        outer:
-        for (int i = 1; i <= 3; i++) {
-            for (int j = 1; j <= 3; j++) {
-                if (i == j)
-                    continue outer;
-                System.out.print(i + "" + j + " ");
-            }
-        }
+    Test() {
+        x += 5;
+        System.out.print(x + " ");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        new Test();
+        new Test();
+        new Test();
     }
 }`
   ];
