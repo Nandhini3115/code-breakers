e7e426e4a65213765961fc9d3af58d3f74db2135
Nandhini3115 <jnandhini3115@gmail.com>
2026-02-26 16:39:09 +0530

    Updates

--- assets/index-DvbRD9R3.js
+++ assets/index-DvbRD9R3.js
@@ -97,12 +97,7 @@ public class Main {
 4 3 3 3 3 3 4 
 4 3 3 3 3 3 4 
 4 4 4 4 4 4 4 `,marks:14}],K1={python:`# Python example for Q1
-n = 1
-for i in range(1, 6):
-    for j in range(i):
-        print(n, end=' ')
-        n += 1
-    print()`,c:`// C example
+{Your code here}`,c:`// C example
 #include <stdio.h>
 int main() {
     // your code here
