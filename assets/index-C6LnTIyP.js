44fa6898ffe74335d7ee2b9b18961384972ab2fd
Nandhini3115 <jnandhini3115@gmail.com>
2026-03-03 20:04:08 +0530

    Updates

--- assets/index-C6LnTIyP.js
+++ assets/index-C6LnTIyP.js
@@ -54,7 +54,7 @@ int main() {
     char str[]="DeBug";
     int i;
 
-    for(i=0;str[i]!="\0";i++){
+    for(i=0;str[i]!=\0;i++){
         if(str[i]>='A' && str[i]<='Z')
             str[i]=str[i]+32;
         else if(str[i]>='a' && str[i]<='z')
